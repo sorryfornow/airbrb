1eabaadcefef7edbfb1c5d656325b23e04a6e84f
wuersen <75011177+wuersen@users.noreply.github.com>
2023-10-30 16:53:21 +1100

    home/login routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
 
+const Home = () => {
+  return <div>Home</div>
+}
+const Login = () => {
+  return <div>login</div>
+}
+
+const Nav = () => {
+  return (
+    <>
+      <span> <Link to="/">Home</Link> </span>
+      <span> <Link to="/login">login</Link> </span>
+    </>
+  )
+}
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <>
+
+      <BrowserRouter>
+        <Nav />
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
