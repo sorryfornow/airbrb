cd95c571faf1566b38254385fc125a9ea4b33584
wuersen <75011177+wuersen@users.noreply.github.com>
2023-10-30 19:51:14 +1100

    home/login/register links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 import Home from './Home'
 import Login from './Login'
+import Register from './Register'
 import Button from '@mui/material/Button';
 
 const Nav = () => {
@@ -14,6 +15,7 @@ const Nav = () => {
     <>
       <span> <Link to="/">Home</Link> </span>
       <Button variant="contained"> <Link to="/login">login</Link> </Button>
+      <Button variant="contained"> <Link to="/register">register</Link> </Button>
     </>
   )
 }
@@ -27,6 +29,7 @@ function App () {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
