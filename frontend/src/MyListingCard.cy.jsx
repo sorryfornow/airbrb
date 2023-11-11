6bbb3b5a9bc145cd3af1d62132f7a6b76557dd46
wuersen <75011177+wuersen@users.noreply.github.com>
2023-11-11 12:47:45 +1100

    1st cypress component test

--- frontend/src/MyListingCard.cy.jsx
+++ frontend/src/MyListingCard.cy.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import MyListingCard from './MyListingCard'
+import {
+  MemoryRouter } from 'react-router-dom'
+let testData;
 
 describe('<MyListingCard />', () => {
+  before(function () {
+    cy.fixture('example').then(function (testdata) {
+      console.log('test data cy: ', testdata)
+       testData = testdata
+    })
+})
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<MyListingCard />)
+    const wrapped = <MemoryRouter>{<MyListingCard data={testData.property1}/>}</MemoryRouter>
+
+    console.log('fixture', testData.property1)
+    cy.mount(wrapped)
   })
 })
