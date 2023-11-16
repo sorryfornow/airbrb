fbed1b2243a22859138a584d61dd44149334111e
wuersen <75011177+wuersen@users.noreply.github.com>
2023-11-16 17:26:47 +1100

    Home displays only published listing

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css'
 import ListingCard from './ListingCard'
 import Search from './Search'
 
 const Home = (props) => {
   const { allListings, setAllListings, reload, setReload } = props
+  const [publishedListings, setPublishedListings] = useState([])
+
+  useEffect(() => {
+    const arr = []
+    const listingIDs = allListings.map((l) => l.id)
+    async function getPublished (id, len, idx) {
+      const reqData = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }
+      try {
+        const fetchResponse = await fetch(`http://localhost:5005/listings/${id}`, reqData);
+        const res = await fetchResponse.json();
+        if (res) {
+          res.listing.id = id
+          if (res.listing.published) { arr.push(res.listing) }
+          if (len - 1 === idx) {
+            setPublishedListings(arr)
+          }
+        }
+      } catch (e) {
+        alert(e)
+      }
+    }
+    for (let i = 0; i < listingIDs.length; i++) {
+      getPublished(listingIDs[i], listingIDs.length, i)
+    }
+  }, [allListings])
   return (
     <>
     <div>All Listings:</div>
     <Search allListings={allListings} setAllListings={setAllListings} reload={reload} setReload={setReload}/>
     <div className={styles.listings}>
-    {allListings && allListings.map((l, i) => <ListingCard key={i} data={l} />)}
+    {publishedListings.length > 0 && publishedListings && publishedListings.map((l, i) => <ListingCard key={i} data={l} />)}
     </div>
     </>
   )
